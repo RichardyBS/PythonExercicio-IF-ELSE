24929219bd88a4d8ad68c17bc606db4c0c1fcc0d
RichardyBS <editionplays@gmail.com>
2024-05-20 15:07:31 -0300

    treino python

--- Python if else treino/exercicio10.py
+++ Python if else treino/exercicio10.py
@@ -1,1 +1,7 @@
 """Escreva um programa que lê um valor I, inteiro positivo e 3 valores A, B e C. Se o valor de I é par então calcular e imprimir na tela a média aritmética de A, B e C Caso contrário, se I>10 então calcular e imprimir a média aritmética e ponderada de A, B e C. Os pesos dos valores são respectivamente 2, 3 e 4. """
+
+valor1 = int(input("Digite o primeiro valor:    "))
+
+valorA = int(input("Digite o valor A:    "))
+valorB = int(input("Digite o valor B:    "))
+valorC = int(input("Digite o valor C:    "))
--- Python if else treino/exercicio9.py
+++ Python if else treino/exercicio9.py
@@ -1,13 +1,16 @@
 """Escreva um programa que verifique se uma letra inserida pelo usuário é vogal ou consoante. """
 
-consoante = chr('b','c','d','f','g','j','k','l','m','n','p','q','r','s','t','v','w','x','z')
+consoante = 'b' , 'c' , 'd' , 'f' , 'g' , 'j' , 'k' , 'l' , 'm' , 'n' , 'p' , 'q' , 'r' , 's' , 't' , 'v' , 'w' , 'x' , 'z'
 
-vogal = chr('a','e','i','o','u')
+vogal = 'a' , 'e' , 'i' , 'o' , 'u'
 
 letra = chr(input("Digite uma Letra, e irei dizer se ela e VOGAL ou CONSOANTE"))
 
-if letra 'b','c','d','f','g','j','k','l','m','n','p','q','r','s','t','v','w','x',"z":
+if letra == consoante:
   print("e uma consoate")
 
-else:
+elif letra == vogal:
   print("e uma vogal")
+
+else:
+  print("Desculpe nao consegui entender")
